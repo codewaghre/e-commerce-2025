4bc885de887b4d3c0f7d041586b8fdcf72c24a8e
Waghre Abhishek <waghreabhishek@gmail.com>
2025-03-26 22:15:16 +0530

    e-commerce

--- e-com-server/src/app.ts
+++ e-com-server/src/app.ts
@@ -63,7 +63,7 @@ app.use(cookieParser())
 
 
 app.get("/", (req, res) => {
-  res.send("API Working with /api/v1 hello test v22");
+  res.send("API Working with /api/v1 hello0000000");
 });
 
 // all Routes
